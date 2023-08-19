b8e0619e9a133a7ec1ddf7e57c42f26580508093
Prashanth.G <prashanthg300@gmail.com>
2023-08-19 13:20:03 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-alphabet = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-]
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def ceasar(input_text, shift_amount, direction):
-  if direction == 'encode':
-    plain_text = input_text
-    cipher_text = ''
-    for letter in plain_text:
-      cipher_text += alphabet[alphabet.index(letter) + shift_amount]       
-    print(f'The encoded text is {cipher_text}')
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+
+from art import logo
+print(logo)
+
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift % 26
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
     
-  elif direction == 'decode': 
-    cipher_text = input_text
-    plain_text = ''
-    for letter in cipher_text:
-      plain_text += alphabet[alphabet.index(letter) - shift_amount]       
-    print(f'The decoded text is {plain_text}')
 
-condition = 'yes'
-while condition == 'yes':
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-  if direction != "encode" or direction != "decode":
-    print("Please enter valid function")
-  else:
-    text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
-    ceasar(input_text = text, shift_amount= shift, direction=direction )
-    condition = input("Type 'yes' if you want to go again. Otherwise type 'no'\n").lower()
+
